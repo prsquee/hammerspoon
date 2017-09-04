5e5aede2e92a50c50693c57cfd19eca3060a54c4
sQuEE <github@fnord.com.ar>
2017-09-03 22:15:45 -0300

    correct name

--- hackintosh/init.lua
+++ hackintosh/init.lua
@@ -3,7 +3,7 @@ require('hyper')
 require('mute_mic')
 
 hostname = hs.host.localizedName()
-if (hostname == "Magic-Sierra") then
+if (hostname == "Magic Sierra") then
   require('audio_switch_hackintosh')
 end
 
