3a1a636ed67bc750b9ddc35c81c7b31f1aefaf4f
prsquee <github@fnord.com.ar>
2018-02-06 14:53:56 -0300

    cosmetics

--- mbpAudioSwitch.lua
+++ mbpAudioSwitch.lua
@@ -1,33 +1,34 @@
 -- audio stuff on macbook pro
 
-local airpodsIcon = hs.image.imageFromPath("airpods.png")
-local outputIcon = hs.menubar.new()
+-- {{{ vars
+local airpodsIcon    = hs.image.imageFromPath("airpods.png"):setSize({w=16,h=16})
+local speakersIcon   = hs.image.imageFromPath("speakers.png"):setSize({w=16,h=16})
+local headphonesIcon = hs.image.imageFromPath("headphones.png"):setSize({w=16,h=16})
+local outputIcon = hs.menubar.new():setTitle(nil)
 
 local builtinOutput = hs.audiodevice.findDeviceByName("Built-in Output")
 local digitalOutput = nil
 local hotkeyToggle  = hs.hotkey.new(hyper, "a", function() toggleOutput() end)
-
+-- }}}
 -- {{{ general watcher for dev# dIn dOut events
 function audiowatch(arg)
-  print("Audiowatch arg: ", arg)
-  print(hs.audiodevice.defaultOutputDevice():name())
-  print(hs.audiodevice.defaultOutputDevice():currentOutputDataSource():name())
+  -- print("Audiowatch arg: ", arg)
+  -- print(hs.audiodevice.defaultOutputDevice():name())
   if arg == "dev#" then
     print("Audiowatch arg: ", arg)
     print(hs.audiodevice.defaultOutputDevice():name(), hs.audiodevice.defaultInputDevice():name())
     -- if a new input device is added, like the airpods, start a watcher for it
     spoon.MuteMic:startInputWatchers()
     setupAudioOutputToggle()
-  end
 
-  if arg == "dIn " then
+  elseif arg == "dIn " then
     if hs.audiodevice.defaultInputDevice():inputMuted() then
       spoon.MuteMic:setMenuBarIcon("mute")
     else
       spoon.MuteMic:setMenuBarIcon("unmute")
     end
-  end
-  if (arg == "dOut") then
+
+  elseif arg == "dOut" then
     setOutputIcon()
     if not hotkeyToggle then
       setupAudioOutputToggle()
@@ -37,27 +38,24 @@ end
 -- }}}
 -- {{{ menubar icon setting
 function setOutputIcon()
-  if hs.audiodevice.defaultOutputDevice():name() == "Built-in Output" then
-    if hs.audiodevice.defaultOutputDevice():currentOutputDataSource():name() == 'Headphones' then
-      outputIcon:setIcon(nil)
-      outputIcon:setTitle('🎧')
+  local current = hs.audiodevice.defaultOutputDevice()
+  if current:name() == "Built-in Output" then
+    if current:currentOutputDataSource():name() == 'Headphones' then
+      outputIcon:setIcon(headphonesIcon)
       outputIcon:returnToMenuBar()
     else
       outputIcon:removeFromMenuBar()
     end
-  elseif hs.audiodevice.defaultOutputDevice():name():match("AirPods") then
-    outputIcon:setTitle(nil)
-    outputIcon:setIcon(airpodsIcon:setSize({w=16,h=16}))
+  elseif current:name():match("AirPods") then
+    outputIcon:setIcon(airpodsIcon)
     outputIcon:returnToMenuBar()
-  elseif (hs.audiodevice.defaultOutputDevice():name() == "DisplayPort" or
-          hs.audiodevice.defaultOutputDevice():name() == "HDMI") then
-    outputIcon:setIcon(nil)
-    outputIcon:setTitle('🔊')
+  elseif (current:name() == "DisplayPort" or current:name() == "HDMI") then
+    outputIcon:setIcon(speakersIcon)
     outputIcon:returnToMenuBar()
   end
 end
 -- }}}
--- {{{ individual watchers for each output device
+-- {{{ start watchers for each output device
 function audioOutWatcher()
   print("dev_uid:", dev_uid, "event_name:", event_name, "event_scope:",event_scope, "event_element:",event_element)
   print(hs.audiodevice.findDeviceByUID(dev_uid):name())
