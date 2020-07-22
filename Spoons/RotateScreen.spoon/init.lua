c1c99b91259857a1d3a3d806976bcf2757044327
sQuEE <github@fnord.com.ar>
2020-07-22 20:55:30 -0300

    better rotatescreen handling

--- Spoons/RotateScreen.spoon/init.lua
+++ Spoons/RotateScreen.spoon/init.lua
@@ -1,15 +1,17 @@
+-- ROTATE external display
 local obj = {}
+
 obj.__index = obj
 
 -- metadata
 obj.name = "RotateScren"
-obj.version = "0.1"
+obj.version = "0.2"
 obj.author = "sQuEE"
 obj.homepage = "https://github.com/prsquee/hammerspoon"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 local screen = nil
-
+local watcher = nil
 function obj:bindHotkeys(mapping)
   if (self.hotkeyToggle) then
     self.hotkeyToggle:delete()
@@ -20,15 +22,18 @@ function obj:bindHotkeys(mapping)
   return self
 end
 
-function obj:start(screenObjFromConfig)
-  if self.hotkeyToggle then
+function obj:start(secondScreen)
+  if secondScreen and self.hotkeyToggle then
+    print('setting up shortcut for rotating second display')
     self.hotkeyToggle:enable()
+    screen = secondScreen
   end
-  screen = screenObjFromConfig
   return self
 end
 
+
 function obj:stop()
+  print('second display disconnedted. Removing keyboard shortcut')
   if self.hotkeyToggle then
     self.hotkeyToggle:disable()
   end
--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ hyper = {"ctrl", "alt", "shift"}
 hypercmd = {"ctrl", "alt", "shift", "cmd"}
 require('auto_reloader')
 require('hyper')
+secondScreen = nil
 
 -- hs.loadSpoon("MuteMic")
 -- spoon.MuteMic:bindHotkeys({toggle={hyper, "f"}})
@@ -12,35 +13,43 @@ spoon.Coffee:bindHotkeys({toggle={hyper, "m"}, startSaver={hyper, "s"} })
 spoon.Coffee:start()
 hostname = hs.host.localizedName()
 if hostname == "Magic Catalina" then
-  -- require('audio_switch_hackintosh')
-  -- print('hackintosh')
-  hs.loadSpoon("AudioSwitch")
-  spoon.AudioSwitch:bindHotkeys({toggle={hyper, "a"}})
-  spoon.AudioSwitch:start()
+  require("lights")
+end
+-- hs.loadSpoon("AudioSwitch")
+-- spoon.AudioSwitch:bindHotkeys({toggle={hyper, "a"}})
+-- spoon.AudioSwitch:start()
 
+screenWatcher = hs.screen.watcher.new(function()
+  if hs.screen'BenQ' then
+    hasSecondScreen()
+  else
+    spoon.RotateScreen:stop()
+  end
+end)
+
+function hasSecondScreen()
   hs.loadSpoon("RotateScreen")
   spoon.RotateScreen:bindHotkeys({toggle={hyper, "`"}})
   spoon.RotateScreen:start(hs.screen'BenQ')
-  require("lights")
-else
-  require("mbpAudioSwitch")
 end
 
-hs.loadSpoon("RotateScreen")
-spoon.RotateScreen:bindHotkeys({toggle={hyper, "`"}})
-spoon.RotateScreen:start(hs.screen'BenQ')
+secondScreen = hs.screen'BenQ'
+  if secondScreen then
+    hasSecondScreen()
+  else
+end
 
 hs.loadSpoon('MiroWindowsManager')
 hs.window.animationDuration = 0
 spoon.MiroWindowsManager:bindHotkeys({
-  up = {hyper, "up"},
-  right = {hyper, "right"},
-  down = {hyper, "down"},
-  left = {hyper, "left"},
-  fullscreen = {hyper, "space"},
-  vertical = {hyper, "\\"},
-  resizeLeft = {hypercmd, "left"},
+  up =          {hyper, "up"},
+  right =       {hyper, "right"},
+  down =        {hyper, "down"},
+  left =        {hyper, "left"},
+  fullscreen =  {hyper, "space"},
+  vertical =    {hyper, "\\"},
+  resizeLeft =  {hypercmd, "left"},
   resizeRight = {hypercmd, "right"},
-  resizeUp = {hypercmd, "up"},
-  resizeDown = {hypercmd, "down"}
+  resizeUp =    {hypercmd, "up"},
+  resizeDown =  {hypercmd, "down"}
 })
