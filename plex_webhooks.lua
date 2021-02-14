cfbd171d64e417d7cd0b07a913bb13a23f7cffa7
prsquee <sq@fnord.com.ar>
2021-02-14 14:31:07 -0300

    newlines

--- plex_webhooks.lua
+++ plex_webhooks.lua
@@ -23,12 +23,12 @@ plex = hs.httpserver.new():setPort(50001):setCallback(function(method, path, hea
           if tag == 'movie' then
             local imdb_id = string.match(json.Metadata.guid, "tt%d+")
             local imdb = 'https://www.imdb.com/title/' .. imdb_id
-            local entry = string.format("%s - %s\n\n> %s\n\n[IMDB](%s)", json.Metadata.title, json.Metadata.year, json.Metadata.summary,imdb)
+            local entry = string.format("%s - %s\n\n> %s\n\n[IMDB](%s)\n\n", json.Metadata.title, json.Metadata.year, json.Metadata.summary,imdb)
             dayone_cmd = dayone_cmd .. tag .. ' -- new ' .. '"' .. entry .. '"'
           end
           if tag == 'episode' then
             local tvdb = 'https://thetvdb.com/series/' ..  json.Metadata.grandparentTitle:gsub(' ', '-'):lower() ..  '/seasons/official/' .. json.Metadata.parentIndex
-            local entry = string.format("%s - S%02dE%02d - %s\n\n[TVDB](%s)", json.Metadata.grandparentTitle, json.Metadata.parentIndex,json.Metadata.index, json.Metadata.title, tvdb)
+            local entry = string.format("%s - S%02dE%02d - %s\n\n[TVDB](%s)\n\n", json.Metadata.grandparentTitle, json.Metadata.parentIndex,json.Metadata.index, json.Metadata.title, tvdb)
             dayone_cmd = dayone_cmd .. tag .. ' -- new ' .. '"' .. entry .. '"'
           end
           --log.d(dayone_cmd)
